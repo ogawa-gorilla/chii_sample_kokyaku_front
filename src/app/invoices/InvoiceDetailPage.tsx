bdb9931f1518a0a24b0afc44e4836011dec7f031
Go <goyoshino0612@icloud.com>
2025-06-02 11:16:20 +0900

    戻るボタンを追加

--- src/app/invoices/InvoiceDetailPage.tsx
+++ src/app/invoices/InvoiceDetailPage.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useAppSelector } from "@/hooks";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { setCurrentPage } from "@/store/navigationSlice";
+import { Page } from "@/types/page";
 import { Button, Container } from "react-bootstrap";
 import { InvoiceDetailCard } from "./components/InvoiceDetailCard";
 
 export const InvoiceDetailPage = () => {
-
+  const dispatch = useAppDispatch();
   const selectedInvoiceId = useAppSelector(state => state.invoice.selectedInvoiceId);
   const invoice = useAppSelector(state => 
     state.invoice.invoices.find(c => c.id === selectedInvoiceId)
@@ -30,6 +32,9 @@ export const InvoiceDetailPage = () => {
       </style>
       <div className="fixed-header-container">
           <nav className="navbar">
+            <Button variant="primary" size="sm" onClick={() => dispatch(setCurrentPage(Page.invoiceList))}>
+              ← 戻る
+            </Button>
             <span className="navbar-brand mb-0 h5">請求書詳細</span>
             <Button size="sm" variant="primary">編集</Button>
           </nav>
